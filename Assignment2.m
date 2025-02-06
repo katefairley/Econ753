fe5e211a006d4a94c031bdd90c7f5d6b5c9de439
katefairley <katy.fairley@gmail.com>
2025-02-06 14:10:59 -0500

    Updated cleaner assignment 2 code

--- Assignment2.m
+++ Assignment2.m
@@ -1,207 +1,225 @@
 % Assignment 2 Econ 753
+% Set working directory
 cd('C:\Users\KatyK\University of Michigan Dropbox\Katherine Fairley\Notability\Spring 2025\753 - Methods\Github')
 
-% Question 1
+% Load and prepare data
+data = csvread('psychtoday.csv', 1);  % Skip header row
+y = data(:,1);                        % Dependent variable
+X = data(:,2:end);                    % Independent variables
+beta_init = zeros(6,1);               % Initial parameter values
 
-data = csvread('psychtoday.csv',1);
+%% Question 1.1: Quasi-Newton with BFGS and numerical derivative
+[beta_numeric, output_numeric, time_numeric] = maximize_numgrad(X, y, beta_init);
 
-y = data(:,1);
-X = data(:,2:end);
+%% Question 1.2: Quasi-Newton with BFGS and analytical derivative
+[beta_analytic, output_analytic, time_analytic] = maximize_analyticgrad(X, y, beta_init);
 
-B0 = zeros(6,1);
-
-
-%% Question 1.1 Quasi-Newton with BFGS and a numerical derivative
-[B_estimated_numeric, output_numeric, time_numeric] = max_numgrad(X, y, B0)
-
-%% Question 1.2 Quasi-Newton with BFGS and an analytical derivative
-[B_estimated_analytic, output_analytic, time_analytic] = max_analyticgrad(X, y, B0)
-
-%% Question 1.3 Nelder-Mead
-obj_fun = @(B) -loglikelihood(X, y, B); % Define the objective function
+%% Question 1.3: Nelder-Mead Optimization
+% Define objective function (negative log-likelihood for minimization)
+obj_fun = @(beta) -loglikelihood(X, y, beta);
 options = optimset('TolX', 1e-12, 'MaxFunEvals', 1e4);
 
-tic; % Start timer
-[B_estimated_NM, fval, exitflag, output] = fminsearch(obj_fun, B0,options) % Use fminsearch to maximize ll
-time_NM = toc;
-output_NM = output;
+tic;  % Start timer
+[beta_nm, fval, exitflag, output] = fminsearch(obj_fun, beta_init, options);
+time_nm = toc;
+output_nm = output;
 
-%% Question 1.4
-[beta_est, conv] = bhhh_poisson(X, y, B0)
+%% Question 1.4: BHHH Algorithm
+[beta_bhhh, convergence_flag] = bhhh_poisson(X, y, beta_init);
 
-%% Question 3
-B = B0;
-n = length(y);
-k = length(B0);
-tol = 1e-12;
+%% Question 3: NLLS Algorithm
+% Initialize parameters
+beta = beta_init;
+n_obs = length(y);
+n_params = length(beta_init);
+tolerance = 1e-12;
 iter_count = 0;
 func_evals = 0;
 tic;
 
-% Initialize residual
-r_old = Inf;
+% Initialize residual for convergence check
+residual_old = Inf;
 converged = false;
 
 while ~converged
     iter_count = iter_count + 1;
     
-    % Calculate residual
-    f = exp(X*B);
-    r = y - f;
+    % Calculate predicted values and residuals
+    pred_values = exp(X * beta);
+    residuals = y - pred_values;
     func_evals = func_evals + 1;
     
-    % Check residual convergence
-    rel_res_change = abs(norm(r) - norm(r_old))/norm(r_old);
-    if rel_res_change < tol && iter_count > 1
+    % Check convergence using relative residual change
+    rel_residual_change = abs(norm(residuals) - norm(residual_old)) / norm(residual_old);
+    if rel_residual_change < tolerance && iter_count > 1
         converged = true;
         break;
     end
-    r_old = r;
+    residual_old = residuals;
     
-    % Calculate the Jacobian
-    J = X .* f;
+    % Calculate Jacobian matrix
+    jacobian = X .* pred_values;
     
-    % Calculate the approximation of the Hessian
-    H = J'*J;
+    % Calculate approximate Hessian
+    hessian = jacobian' * jacobian;
     
-    % Add regularization if matrix is close to singular
-    if rcond(H) < 1e-12
-        H = H + 1e-4 * eye(k); 
+    % Add regularization if Hessian is near-singular
+    if rcond(hessian) < 1e-12
+        hessian = hessian + 1e-4 * eye(n_params);
     end
     
-    % Calculate step
-    d = H\(J'*r);
-    
-    B_new = B + d;
-    
-    
-    B = B_new;
+    % Calculate parameter update
+    delta = hessian \ (jacobian' * residuals);
+    beta_new = beta + delta;
+    beta = beta_new;
 end
 
-toc;
+time_nlls = toc;
 
+%% Helper Functions
 
-%% Functions
-
-% Maximization with numerical gradient (Q1.1)
-function [B_opt, output, time] = max_numgrad(X, y, B0)
+function [beta_opt, output, time] = maximize_numgrad(X, y, beta_init)
+    % Maximizes log-likelihood using BFGS with numerical gradient
     tic;
+    
     % Set optimization options
     options = optimoptions('fminunc', ...
-        'SpecifyObjectiveGradient', false, ... % Use numerical derivatives by default
+        'SpecifyObjectiveGradient', false, ...    % Use numerical derivatives
         'Algorithm', 'quasi-newton', ...
         'Display', 'iter', ...
         'StepTolerance', 1e-12);
     
-    % Define objective function
-    obj_fun = @(B) -loglikelihood(X, y, B);
+    % Define objective function (negative log-likelihood)
+    obj_fun = @(beta) -loglikelihood(X, y, beta);
     
     % Minimize negative log-likelihood
-    [B_opt, fval, exitflag, output] = fminunc(obj_fun, B0, options)
-
-    %iterations = output.iterations;
-    %func_evals = output.funcCount;
+    [beta_opt, fval, exitflag, output] = fminunc(obj_fun, beta_init, options);
     time = toc;
 end
 
-% Log-likelihood function
-function ll = loglikelihood(X, y, B)
-    ll = (-exp(X*B) + y.*X*B - log(factorial(y))).'*ones(height(y),1);
+function ll = loglikelihood(X, y, beta)
+    % Computes Poisson log-likelihood
+    % Input:
+    %   X: matrix of independent variables
+    %   y: vector of dependent variable
+    %   beta: parameter vector
+    % Output:
+    %   ll: log-likelihood value
+    ll = (-exp(X*beta) + y.*(X*beta) - log(factorial(y))).' * ones(height(y),1);
 end
 
-
-% Maximization with analytical gradient (Q1.2)
-function [B_opt, output, time] = max_analyticgrad(X, y, B0)
-   tic;
-   options = optimoptions('fminunc', ...
-       'SpecifyObjectiveGradient', true, ...
-       'Algorithm', 'quasi-newton', ...
-       'Display', 'iter', ...
-       'StepTolerance', 1e-12);
-   
-   obj_fun = @(B) loglike_with_grad(X, y, B);
-   [B_opt, fval, exitflag, output] = fminunc(obj_fun, B0, options);
-
-   time = toc;
+function [beta_opt, output, time] = maximize_analyticgrad(X, y, beta_init)
+    % Maximizes log-likelihood using BFGS with analytical gradient
+    tic;
+    
+    options = optimoptions('fminunc', ...
+        'SpecifyObjectiveGradient', true, ...     % Use analytical gradient
+        'Algorithm', 'quasi-newton', ...
+        'Display', 'iter', ...
+        'StepTolerance', 1e-12);
+    
+    obj_fun = @(beta) loglike_with_grad(X, y, beta);
+    [beta_opt, fval, exitflag, output] = fminunc(obj_fun, beta_init, options);
+    time = toc;
 end
 
-% Log-likelihood function with analytical gradient specified
-function [f, g] = loglike_with_grad(X, y, B_val)
-    XB = X * B_val;
-    expXB = exp(XB);
-    f = -(-sum(expXB) + y'*XB - sum(log(factorial(y))));
-    g = -(X' * (y - expXB));
+function [f, g] = loglike_with_grad(X, y, beta)
+    % Computes log-likelihood and its gradient
+    % Input:
+    %   X: matrix of independent variables
+    %   y: vector of dependent variable
+    %   beta: parameter vector
+    % Output:
+    %   f: negative log-likelihood value
+    %   g: gradient vector
+    
+    X_beta = X * beta;
+    exp_X_beta = exp(X_beta);
+    f = -(-sum(exp_X_beta) + y'*X_beta - sum(log(factorial(y))));
+    g = -(X' * (y - exp_X_beta));
 end
 
-
-% Score function
 function score = score_function(beta, X, y)
+    % Computes score (gradient of log-likelihood)
     score = X' * (y - exp(X*beta));
 end
 
-% Do BHHH Algorithm
 function [beta, converged] = bhhh_poisson(X, y, beta_init)
-   tol = 1e-6;
-   beta = beta_init;
-   n = length(y);
-   
-   iter_count = 0;
-   func_evals = 0;
-   tic;
-   
-   % Calculate initial Hessian approximation and eigenvalues
-   mu_init = exp(X*beta_init);
-   scores_init = zeros(n, length(beta_init));
-   for i = 1:n
-       scores_init(i,:) = X(i,:)' * (y(i) - mu_init(i));
-   end
-   B_init = scores_init' * scores_init;
-   eig_init = eig(B_init);
-   
-   beta_new = beta + 2*tol;
-   
-   while norm(beta_new - beta) >= tol
-       iter_count = iter_count + 1;
-       beta = beta_new;
-       
-       mu = exp(X*beta);
-       ll = (-mu + y.*X*beta - log(factorial(y))).'*ones(height(y),1);
-       func_evals = func_evals + 1;
-       
-       scores = zeros(n, length(beta));
-       for i = 1:n
-           scores(i,:) = X(i,:)' * (y(i) - mu(i));
-       end
-       
-       B = scores' * scores;
-       total_score = sum(scores, 1)';
-       
-       if rcond(B) < 1e-12
-           B = B + 1e-6 * eye(size(B));
-       end
-       delta = B \ total_score;
-       beta_new = beta + delta;
-   end
-   
-   % Calculate final Hessian approximation and eigenvalues
-   mu = exp(X*beta_new);
-   scores_fin = zeros(n, length(beta));
-   for i = 1:n
-       scores_fin(i,:) = X(i,:)' * (y(i) - mu(i));
-   end
-       
-   B = scores_fin' * scores_fin;
-
-   eig_final = eig(B);
-   
-   elapsed_time = toc;
-   fprintf('Converged in %d iterations\n', iter_count);
-   fprintf('Function evaluations: %d\n', func_evals);
-   fprintf('Elapsed time: %.4f seconds\n', elapsed_time);
-   fprintf('\nEigenvalues of initial Hessian approximation:\n');
-   disp(eig_init);
-   fprintf('\nEigenvalues of final Hessian approximation:\n');
-   disp(eig_final);
-   converged = true;
+    % Implements BHHH algorithm for Poisson regression
+    % Input:
+    %   X: matrix of independent variables
+    %   y: vector of dependent variable
+    %   beta_init: initial parameter values
+    % Output:
+    %   beta: estimated parameters
+    %   converged: convergence flag
+    
+    tolerance = 1e-6;
+    beta = beta_init;
+    n_obs = length(y);
+    
+    iter_count = 0;
+    func_evals = 0;
+    tic;
+    
+    % Calculate initial Hessian approximation and eigenvalues
+    mu_init = exp(X*beta_init);
+    scores_init = zeros(n_obs, length(beta_init));
+    for i = 1:n_obs
+        scores_init(i,:) = X(i,:)' * (y(i) - mu_init(i));
+    end
+    hessian_init = scores_init' * scores_init;
+    eig_init = eig(hessian_init);
+    
+    beta_new = beta + 2*tolerance;
+    
+    while norm(beta_new - beta) >= tolerance
+        iter_count = iter_count + 1;
+        beta = beta_new;
+        
+        % Calculate predicted values and log-likelihood
+        mu = exp(X*beta);
+        ll = (-mu + y.*X*beta - log(factorial(y))).' * ones(height(y),1);
+        func_evals = func_evals + 1;
+        
+        % Calculate scores for each observation
+        scores = zeros(n_obs, length(beta));
+        for i = 1:n_obs
+            scores(i,:) = X(i,:)' * (y(i) - mu(i));
+        end
+        
+        % Calculate BHHH Hessian approximation
+        hessian = scores' * scores;
+        total_score = sum(scores, 1)';
+        
+        % Add regularization if Hessian is near-singular
+        if rcond(hessian) < 1e-12
+            hessian = hessian + 1e-6 * eye(size(hessian));
+        end
+        
+        % Update parameters
+        delta = hessian \ total_score;
+        beta_new = beta + delta;
+    end
+    
+    % Calculate final Hessian approximation and eigenvalues
+    mu_final = exp(X*beta_new);
+    scores_final = zeros(n_obs, length(beta));
+    for i = 1:n_obs
+        scores_final(i,:) = X(i,:)' * (y(i) - mu_final(i));
+    end
+    
+    hessian_final = scores_final' * scores_final;
+    eig_final = eig(hessian_final);
+    
+    % Print summary statistics
+    elapsed_time = toc;
+    fprintf('Converged in %d iterations\n', iter_count);
+    fprintf('Function evaluations: %d\n', func_evals);
+    fprintf('Elapsed time: %.4f seconds\n', elapsed_time);
+    fprintf('\nEigenvalues of initial Hessian approximation:\n');
+    disp(eig_init);
+    fprintf('\nEigenvalues of final Hessian approximation:\n');
+    disp(eig_final);
+    converged = true;
 end
-
